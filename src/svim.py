39bde615a5bf98318b4e8b0babfa7090caf08fbb
Sebastian Wong <wngsebastian@gmail.com>
2014-03-17 15:40:08 -0500

    Beginning of input router logic

--- src/svim.py
+++ src/svim.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 from Tkinter import Tk
 from Graphics import text_canvas
+from Input import user_input
 
 if __name__ == '__main__':
     root = Tk()
-    app = text_canvas(root, 20)
-    app.write_text_grid(0, 0,'lolo')
-    app.write_text_grid(0, 1,'lolololololololol')
-    app.write_text_grid(3, 5,'lolololololololol')
-    app.draw_grid_lines(0)
+    input_handler = user_input.user_input()
+    app = text_canvas.text_canvas(root, 12, input_handler)
     root.mainloop()
