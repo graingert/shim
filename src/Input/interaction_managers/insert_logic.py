2e827048008ddb7fabdf9147f8a3d40fbf9beca2
Sebastian Wong <wngsebastian@gmail.com>
2014-03-22 15:43:20 -0500

    removing useless files

--- src/Input/interaction_managers/insert_logic.py
+++
@@ -1,6 +0,0 @@
-def insert_text_str(s, instance):
-    curr_top = instance.get_curr_top()
-    x, y = instance.get_cursor()
-    curr_line = instance.get_line(y + curr_top)
-    instance.set_line(curr_top + y, curr_line[:x] + s + curr_line[x:])
-    instance.set_cursor(x + len(s), y)
